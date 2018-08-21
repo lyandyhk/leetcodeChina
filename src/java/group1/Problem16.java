f65baf4e45663ebd188d8e9c6f0b0dabe4a032a0
liuyu51 <liuyu51@meituan.com>
2018-08-21 20:21:53 +0800

    update 16

--- src/java/group1/Problem16.java
+++ src/java/group1/Problem16.java
@@ -5,12 +5,8 @@ import java.util.Arrays;
 public class Problem16 {
     public static int threeSumClosest(int[] nums, int target) {
         Arrays.sort(nums);
-        int realResult = -1,singleRealResult = -1;
-        //最后的差值最小的值
-        int result = -1;
-
-        //单趟的最小差值
-        int singleAbsResult = -1;
+        int result = Integer.MAX_VALUE;
+        int res = 0;
         int curr,x,y;
         int currTarget;
         //单次target相减后的数,有正负
@@ -20,46 +16,31 @@ public class Problem16 {
             x = i + 1;
             y = nums.length - 1;
             currTarget = target - curr ;
-            currResult = currTarget - nums[x] - nums[y];
-            if(currResult == 0)
-                return nums[x] + nums[y] + curr;
-            singleAbsResult = Math.abs(currResult);
-            singleRealResult = nums[x] + nums[y] + curr;
             while(x<y){
                 currResult = currTarget - nums[x] - nums[y];
                 if(currResult == 0)
                     return nums[x] + nums[y] + curr;
                 if(currResult < 0){
-                    if(Math.abs(currResult) < singleAbsResult){
-                        singleAbsResult = Math.abs(currResult);
-                        singleRealResult = nums[x] + nums[y] + curr;
+                    if(-1 * currResult < result){
+                        result = -1 * currResult;
+                        res = curr + nums[x] + nums[y];
                     }
                     y--;
-                    continue;
                 }
-                if(currResult > 0){
-                    if(Math.abs(currResult) < singleAbsResult){
-                        singleAbsResult = Math.abs(currResult);
-                        singleRealResult = nums[x] + nums[y] + curr;
+                else if(currResult > 0){
+                    if(currResult < result){
+                        result = currResult;
+                        res = curr + nums[x] + nums[y];
                     }
                     x++;
-                    continue;
                 }
             }
-            if(result == -1){
-                result = singleAbsResult;
-                realResult = singleRealResult;
-                continue;
-            } else if(singleAbsResult < result){
-                result = singleAbsResult;
-                realResult = singleRealResult;
-            }
         }
-        return realResult;
+        return res;
     }
 
     public static void main(String[] args) {
-        int[] arr = new int[]{0,2,1,-3};
-        System.out.println(threeSumClosest(arr,1));
+        int[] arr = new int[]{-1,0,1,1,55};
+        System.out.println(threeSumClosest(arr,3));
     }
 }
